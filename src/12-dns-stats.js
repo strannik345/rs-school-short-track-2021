b7cd8cbc2fe3f2ec537dc1990fa879bea1518c37
strannik345 <volodkovasya@gmail.com>
2021-04-12 20:45:27 +0300

    feat: add 12

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,20 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const list = {};
+  for (let i = 0; i < domains.length; i++) {
+    const current = domains[i].split('.').reverse().map((e) => `.${e}`);
+    for (let j = 0; j < current.length; j++) {
+      let domain = '';
+      for (let k = 0; k <= j; k++) {
+        domain += current[k];
+      }
+      if (!list[domain]) list[domain] = 1;
+      else list[domain]++;
+    }
+  }
+  return list;
 }
 
 module.exports = getDNSStats;
